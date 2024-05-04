ba33d0ca711d4f008727321741299f7e6ae0915b
cmaughan787 <mau24003@byui.edu>
2024-05-03 19:54:49 -0600

    finished 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,30 +1,44 @@
 using System;
+using System.Net;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Please enter the magic number");
-    
-        string numberStr = Console.ReadLine();
-        int number = int.Parse(numberStr);
+    Random randomGenerator = new Random();
+     int number = randomGenerator.Next(1,100);
 
         int guess = 0;
-        do
+        int numberGuesses = 0;
+        string response = "";
+        do 
         {
+            do
+            {
 
-        Console.WriteLine("Enter a guess");
-        string guessStr = Console.ReadLine();
-        guess = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter a guess between 1 and 101");
+            string guessStr = Console.ReadLine();
+            guess = int.Parse(Console.ReadLine());
+            numberGuesses++; 
+        
 
-        if (guess>number)
-        {
-          Console.WriteLine("Lower");  
-        }
-        else if (guess<number)
-        {
-            Console.WriteLine("Higher");
-        }
-        } while (guess!= number);
+            if (guess>number)
+            {
+            Console.WriteLine("Lower"); 
+            }
+            else if (guess<number)
+            {
+                Console.WriteLine("Higher"); 
+            }
+            else 
+            {
+                Console.WriteLine($"You guessed it with {numberGuesses} guesses!");
+            }
+            } while (guess!= number);
+
+        Console.Write("Do you want to continue?");
+        response = Console.ReadLine();
+        } while (response == "yes");
     }
 }
