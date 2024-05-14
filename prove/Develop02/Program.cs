5703c65e33ade42097533caad7bb12018985b9d6
cmaughan787 <mau24003@byui.edu>
2024-05-13 20:44:30 -0600

    Beginning

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        Console.WriteLine("Welcome to your journal!");
+        Console.WriteLine("1. Write an entry");
+        Console.WriteLine("2. Display journal");
+        Console.WriteLine("3. Load journal");
+        Console.WriteLine("4. Save new entry");
+        Console.WriteLine("5. Quit");
     }
 }
