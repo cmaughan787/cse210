fd4f6d207a5cb9e1f2b7443e3d0670e203d3a407
cmaughan787 <mau24003@byui.edu>
2024-06-06 22:58:53 -0600

    Guided practice

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+
+        Assignment a1 = new Assignment("Mary Smith", "Division");
+        Console.WriteLine(a1.GetSummary());
+
+
+        MathAssignment a2 = new MathAssignment("John Doe", "Fractions", "6.4", "9-15");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+
+        WritingAssignment a3 = new WritingAssignment("Alice James", "Current Events", "Current Events in US Politics");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
     }
 }
--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -7,3 +7,4 @@ class Program
         Console.WriteLine("Hello Develop04 World!");
     }
 }
+// choose between 1, 2, 3 then ask how long the activity should last 
